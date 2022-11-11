0ff7cc5bdeeaa3152a54e209d99edbbe35324ee4
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2022-11-11 18:06:17 +0100

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,13 +1,13 @@
 import random
 
-lower_case = "abcde23456fghijklmnopqrstuvwxyz"
-upper_case = "ABCDEFGHIJK()-+;][{LMNOPQRSTUVWXYZ"
-numbers = "1234567jklmnop890"
-symbols = "@:!$£ABCDEFGHI%^&*()-+;][{/"
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "1234567890"
+symbols = "@:!$£%^&*()-+;][{/"
 
 use_for = lower_case + upper_case + numbers + symbols
 
-length_for_pass = 17
+length_for_pass = 9
 
 password = "".join(random.sample(use_for, length_for_pass))
 
