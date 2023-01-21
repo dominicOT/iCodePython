dbd34134874f6cd2c94d29127d5aedac885e1c6b
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-01-21 03:04:20 +0100

    Create replaceWord.py

---
+++ string manipualtion/replaceWord.py
@@ -0,0 +1,8 @@
+#21/01/2023
+#Author: dominicOT
+
+wrong_str = "I am not a Python coder"
+right_str = wrong_str.replace("not", "")
+
+print("Initial String : ", wrong_str)
+print("Modified String : ", right_str)
