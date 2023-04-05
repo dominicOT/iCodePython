c1ab4ffb5c5d16533d83a4bbf764bdd9948f11a1
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-04-05 19:39:44 +0100

    Add files via upload

---
+++ string manipualtion/string rep2.py
@@ -0,0 +1,10 @@
+# .... Group 1 - Python
+# .... Strings & String Methods
+# .... String Rep & Cat
+
+
+x_position = input("X: ")
+y_position = input("Y: ")
+
+z = int(x_position) + int(y_position)
+print(z)
