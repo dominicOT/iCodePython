63054d9ef9be490ce97b68355e4bc887a912a0e6
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-06-13 19:09:27 +0100

    Update wifi deactivator.py

--- wifi deactivator.py
+++ wifi deactivator.py
@@ -1,9 +1,1 @@
-##import subprocess 
-##
-##wfCMD = subprocess.run (['netsh', 'interface', 'set', 'interface', "wi-fi", "DISABLED"])
-##wfCMD
 
-
-import subprocess 
-MyWish = subprocess.run (['netsh', 'interface', 'set', 'interface', "wi-fi", "ENABLED"])
-MyWish
