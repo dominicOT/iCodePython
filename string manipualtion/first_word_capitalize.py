e990e54ae128be87ddb2f4bf1d6473eda17b4eba
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-06-13 19:03:00 +0100

    Update and rename word_capitalize.py to first_word_capitalize.py

---
+++ string manipualtion/first_word_capitalize.py
@@ -0,0 +1,8 @@
+def first_word_capitalize(text):
+    capitalized_text = text.capitalize()
+    return capitalized_text
+
+# Example usage
+input_text = input("Enter a word or sentence: ")
+capitalized_text = first_word_capitalize(input_text)
+print("Capitalized text:", capitalized_text)
