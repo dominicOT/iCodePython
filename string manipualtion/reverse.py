89f0a4943700e7cd7e8eeda7025db2ce1c0a84f9
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-06-13 18:44:41 +0100

    Update reverse.py

--- string manipualtion/reverse.py
+++ string manipualtion/reverse.py
@@ -5,6 +5,9 @@ def reverse_string(input_str: str) -> str:
    
    >>> reverse_letters('The quick brown fox jumped over the lazy dog.')
     'ehT kciuq nworb xof depmuj revo eht yzal .god'
+    
+   >>> reverse_letters("I code Python")
+    'I edoc nohtyP'
     """
     return " ".join([word[::-1] for word in input_str.split()])
 
