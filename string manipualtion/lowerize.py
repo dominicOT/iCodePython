aedbe83ffa9b48ab27df9bc0bdd7f99d250848e0
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-06-13 21:01:13 +0100

    Create lowerize.py

---
+++ string manipualtion/lowerize.py
@@ -0,0 +1,8 @@
+def lowerize_words(sentence):
+    lowerized_sentence = sentence.lower()
+    return lowerized_sentence
+
+
+new_sentence = input("")
+lowerized_sentence = lowerize_words(new_sentence)
+print("Lowerized sentence:", lowerized_sentence)
